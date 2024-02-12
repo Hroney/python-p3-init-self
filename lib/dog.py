abe2b8e5f42297b7da5714d7575eb1c2cb14475c
Hayden Roney <roneyhayden@gmail.com>
2024-02-12 08:45:35 -0600

    lab done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Dog:
+
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+        print(f"{name} is born!")
+
+    def bark(self):
+        print("Woof!")
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
     pass
